8071d311c5ddf22f632f42c5dd0abf69f93c8291
Hitalo Loose <hitaloose@gmail.com>
2020-03-30 18:56:21 -0300

    alterando url banco de dados

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   client: 'postgresql',
-  connection: process.env.DB_URL,
+  connection: process.env.DATABASE_URL,
   pool: {
     min: 2,
     max: 10
